80cc2705414b54ba0e8e6c37ced51da5b3a5355d
Ricardo William <billysoftperu@gmail.com>
2021-05-21 01:41:10 -0500

    Configurando container y botones de la portada

--- src/componentes/Portada/Portada.js
+++ src/componentes/Portada/Portada.js
@@ -5,38 +5,42 @@ import './Portada.css';
 
 function Portada() {
     return (
-        <div className='portada-contenedor'>
-           
-           
-            <h1>I.E. Josefa Carrillo y Albornoz</h1>
-            <p>Lurigancho  - Chosica</p>
-            <div className='portada-btns'>
-                <Button
-                    className='btns'
-                    buttonStyle='btn--primary'
-                    buttonSize='btn--large'
-                >
-                    Bienvenidos <i className='far fa-play-circle' />
-                </Button>
+        <div className='portada-container'>
+            <div className="container">
 
-                <Button
-                    className='btns'
-                    buttonStyle='btn--primary'
-                    buttonSize='btn--large'
-                    
-                >
-                    Galería de Fotos <i className='far fa-play-circle' />
-                </Button>
+                <h1>I.E. Josefa Carrillo y Albornoz</h1>
+                <p>Lurigancho  - Chosica</p>
+                <div className='portada-btns'>
+                    <Button
+                        className='btns'
+                        buttonStyle='btn--primary'
+                        buttonSize='btn--large'
+                    >
+                        Bienvenidos <i className='far fa-play-circle' />
+                    </Button>
 
-                <Button
-                    className='btns'
-                    buttonStyle='btn--primary'
-                    buttonSize='btn--large'
-                    
-                >
-                    Facebook <i className='far fa-play-circle' />
-                </Button>
+                    <Button
+                        className='btns'
+                        buttonStyle='btn--primary'
+                        buttonSize='btn--large'
+                        
+                    >
+                        Galería de Fotos <i className='far fa-play-circle' />
+                    </Button>
+
+                    <Button
+                        className='btns'
+                        buttonStyle='btn--primary'
+                        buttonSize='btn--large'
+                        
+                    >
+                        Facebook <i className='far fa-play-circle' />
+                    </Button>
+                </div>
             </div>
+           
+           
+            
         </div>
     );
 }
